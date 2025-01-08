99861ff0639b51d369fc92223e8a85244cde866e
Diego Aspinwall <diegoaspinwall@yale.edu>
2025-01-08 04:08:30 -0500

    added getUserLeaderboard

--- database/functions/src/index.ts
+++ database/functions/src/index.ts
@@ -30,6 +30,13 @@ interface College {
   points: number;
 }
 
+interface User {
+  id: string;
+  firstname: string;
+  lastname: string;
+  points: number;
+}
+
 interface AddBetRequestBody {
   email: string;
   matchId: string;
@@ -256,6 +263,7 @@ export const addBet = functions.https.onRequest(async (req, res) => {
 });
 
 export const deleteBet = functions.https.onRequest(async (req, res) => {
+  // TODO: fix this. There is no ID attached to a bet; they are in an array. It should iterate through all bets to check for common elements. Check that MatchID, betAmount, betOption, betOdds all match
   corsHandler(req, res, async () => {
     const { email, matchId }: DelBetRequestBody = req.body;
 
@@ -401,6 +409,34 @@ export const updateUserLeaderboard = functions.https.onRequest(async (req, res) 
   });
 });
 
+export const getUserLeaderboard = functions.https.onRequest((req, res) => {
+  return corsHandler(req, res, async () => {
+    try {
+      const leaderboardRef = db.collection("user_leaderboard_dha_testing");
+      const snapshot = await leaderboardRef.orderBy("points", "desc").get();
+
+      if (snapshot.empty) {
+        res.status(404).send("No users found");
+        return;
+      }
+
+      const leaderboard: User[] = [];
+      snapshot.forEach((doc) => {
+        const data = doc.data();
+        leaderboard.push({
+          id: doc.id,
+          firstname: data.firstname,
+          lastname: data.lastname,
+          points: data.points,
+        } as User);
+      });
+      res.status(200).json(leaderboard);
+    } catch (error) {
+      console.error("Error fetching leaderboard from Function Code:", error);
+      res.status(500).send("Internal Server Error");
+    }
+  });
+});
 
 export const fetchOrAddUser = functions.https.onRequest((req, res) => {
   corsHandler(req, res, async () => {
